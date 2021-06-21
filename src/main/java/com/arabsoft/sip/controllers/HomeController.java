60f99f36692239202d7e832522437bdbdfbc8b84
mohamed khaldi <mohamed.khaldi@sesame.com.tn>
2021-06-21 16:21:00 +0200

    2eme

--- src/main/java/com/arabsoft/sip/controllers/HomeController.java
+++ src/main/java/com/arabsoft/sip/controllers/HomeController.java
@@ -2,16 +2,24 @@ package com.arabsoft.sip.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/home")
-	@ResponseBody
+	//@ResponseBody // sans vue 
 	public String HomController() {
 	 
-		return "<h1>Spring</h1>";
+		//return "<h1>Spring</h1>";
+		return "home";
+	}
+	@RequestMapping("/liste")
+	//@ResponseBody // sans vue 
+	public String ListCondidat() {
+	 
+		//return "<h1>Spring</h1>";
+		return "liste_condidat";
 	}
 
 }
